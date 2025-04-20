2fe96d3bc8a70839dc95b4e99c6c1eac2ea3b4c9
Andrew Walton <163466371+AndrewASUS@users.noreply.github.com>
2025-04-20 18:42:59 +1000

    fix bug error

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js" // Use .js at the end of file name on local file if useing ("type": "model").
+import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js" 
 import { connectDB } from "./lib/db.js"
 import cookieParser from "cookie-parser"
